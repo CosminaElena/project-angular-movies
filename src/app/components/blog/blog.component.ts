07ed644c9ffddafd1ae33e9d411faff4cc806799
CosminaElena <87190695+CosminaElena@users.noreply.github.com>
2021-10-11 20:57:12 +0300

    blog

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -8,45 +8,48 @@ import { DomSanitizer, SafeUrl, Title } from '@angular/platform-browser';
   styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent implements OnInit {
-  blogs: any;
- 
+  // blogs: any;
 	// public imageUrls: SafeUrl[];
-
 	// private lastObjectUrl: string;
 	// private sanitizer: DomSanitizer
+
+
+blogs: any = []
+
+
   constructor(private titleService: Title,
               // sanitizer: DomSanitizer
   ) {
 
     this.titleService.setTitle('Movies App - Blog');
-
     // this.sanitizer = sanitizer;
-
 		// this.imageUrls = [];
 		// this.lastObjectUrl = "";
   }
 
 
   ngOnInit(): void {
-    let blogList = localStorage.getItem('blogs');
-    if (blogList) {
-      this.blogs = JSON.parse(blogList);
-    }
+   let blogList = localStorage.getItem("blogs");
+  	if(blogList){
+  		this.blogs = JSON.parse(blogList)
+  	}
+
   }
 
-  addBlog(name: any, content: any, image: any) {
+  addBlog(title: any, content: any, image: any) {
     let blog = {
-      name: name.value,
-      content: content.value,
-      image: image.value,
+    title: title.value,
+    content: content.value,
+    image: image.value,
+  
     };
-
-    this.blogs.push(blog);
-    localStorage.setItem('blogs', JSON.stringify(this.blogs));
-
-    console.log(blog.name, blog.content, blog.image);
+    this.blogs.push(blog)
+  	localStorage.setItem("blogs",JSON.stringify(this.blogs));
+    console.log(blog.title, blog.content, blog.image);
+ 
   }
 
+  }
 
   //   this.lastObjectUrl = URL.createObjectURL( pastedImage );
   //   this.imageUrls.unshift(
@@ -57,7 +60,6 @@ export class BlogComponent implements OnInit {
 
   
   
-  
+
 	
 
-}
